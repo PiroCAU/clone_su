8647177d132b2bd8654bc9f36c9053f2214ef13e
leesuyong849 <toosee1005@cau.ac.kr>
2025-04-01 15:10:51 +0900

    FEAT: apiResponse 관련 설정 완료

---
+++ src/main/java/clone/carrotMarket/apiPayload/ApiResponse.java
@@ -0,0 +1,52 @@
+package clone.carrotMarket.apiPayload;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponse<T> {
+
+    @Schema(description = "상태 코드", example = "200")
+    private int status;
+
+    @Schema(description = "에러 발생시 에러코드", example = "S500")
+    private int code;
+
+    @Schema(description = "응답 메세지")
+    private String message;
+
+    @Schema(description = "응답 데이터")
+    private T data;
+
+    //성공 시 응답코드 반환
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return ApiResponse.<T>builder()
+                .status(200)
+                .code(null)
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    //실패 시 응답코드 반환
+    public static <T> ApiResponse<T> error(ErrorCode errorCode) {
+        return ApiResponse.<T>builder()
+                .status(errorCode.getStatus)
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .data(null)
+                .build();
+    }
+
+    // 에러 응답 생성 메서드(커스텀 메세지 추가)
+    public static <T> ApiResponse<T> error(String message ,ErrorCode errorCode, T data) {
+        return ApiResponse.<T>builder()
+                .status(errorCode.getStatus())
+                .code(errorCode.getCode())
+                .message(message)
+                .data(data)
+                .build();
+    }
+}
